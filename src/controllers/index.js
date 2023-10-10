37225335d1a101cb32bbc41ade5bdb892820c293
dhrumil <asodariyadhrumil830@gmail.com>
2023-10-10 15:07:27 +0530

    final

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,3 @@
 module.exports.userController = require("./user.controller.js");
-module.exports.categoryController = require("./category.controller.js");
-module.exports.productController = require("./product.controller.js");
 module.exports.orderController = require("./order.controller.js");
 module.exports.cartController = require("./cart.controller.js");
